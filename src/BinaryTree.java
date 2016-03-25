ff01b8947cf5cf971d3380fc4a47f9d282851d64
Stephen <bold-scot@hotmail.com>
2016-03-25 18:43:25 +0000

    Implemented encodeAChar and encodeText methods.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -11,7 +11,7 @@ public class BinaryTree {
 	private ArrayList<Node> forrest;
 	private ArrayList<Character> charArray;
 	private final String TEST_STRING= "Hello World, Good Morning! How is everyone doing?";
-
+	
 	public BinaryTree() {
 		Main();
 	}
@@ -20,8 +20,10 @@ public class BinaryTree {
 		forrest = new ArrayList<>();
 		charArray = new ArrayList<>();
 		buildTree();
+		
+		encodeText(TEST_STRING);
 	}
-
+	
 	private void buildTree() {
 		//Add all characters from the string to the charArray.
 		for (int index = 0; index < TEST_STRING.length(); ++index) {
@@ -51,7 +53,7 @@ public class BinaryTree {
 			}
 		}
 	}
-	
+
 	/*
 	 * This method links the nodes to create the tree, the two lowest weighted nodes data and weight are used to make
 	 * a root node. The root node has the lowest nodes as it's left and right node links and those nodes have the new node as their root node.
@@ -59,7 +61,7 @@ public class BinaryTree {
 	 */
 	private void linkNodes() {
 		Collections.sort(forrest);
-		
+
 		int counter = forrest.size()-1;
 		while (counter != 0) {
 			Node lowest = forrest.get(counter);
@@ -70,34 +72,64 @@ public class BinaryTree {
 			newNode.setLeft(lowest);
 			newNode.setRight(secondLowest);
 			System.out.println("The two lowest weight nodes: " + "\n" + lowest + secondLowest + "\n" + "The new node: " + newNode);
-			
+
 			//Set the root on both the nodes to the new node.
 			lowest.setRoot(newNode);
 			secondLowest.setRoot(newNode);
-			
+
 			//Add the new node to the forrest ArrayList at index 0 and sort a sublist of the list.
 			//The sublist decreases by one each iteration.
 			forrest.add(0, newNode);
 			Collections.sort(forrest.subList(0, counter));
 		}
-		
+
 		treeRootNode = forrest.get(0);
 		System.out.println("This is the root of the tree: " + treeRootNode);
-		
+
 	}
 	
-	/*private String encodeString(String text) {
-		Node currentNode = treeRootNode;
-		String encodedText = "";
-		char currentCh = ' ';		
-		for (int stringIndex = 0; stringIndex < text.length(); ++stringIndex) {
-			
-		}*/
+	private void encodeText(String text) {
+		String encodedString = "";
+		for (int index = 0; index < text.length(); ++index) {
+			encodedString+=encodeACharacter(text.charAt(index));
+		}
 		
+		System.out.println("This is the encoded text: " + encodedString);
+	}
+	
+	private void decodeString(String str) {
 		
+	}
+
+	private String encodeACharacter(char ch) {
+		Node currentNode = null;
+		String encodedChar= "";
+		char currentCh = ch ;
 		
+		if (treeRootNode.getLeft().getData().contains(String.valueOf(currentCh) ) ) {
+			currentNode = treeRootNode.getLeft();
+			encodedChar += "0";
+		}
+		else {
+			currentNode = treeRootNode.getRight();
+			encodedChar += "1";
+		}
 		
-		return text;
+		while (currentNode.getLeft() != null && currentNode.getRight() != null ) {
+			if (currentNode.getData().contains(String.valueOf(currentCh)) ) {
+				if (currentNode.getData().equals(String.valueOf(currentCh)) ){
+					return encodedChar;
+				}
+				else if (currentNode.getLeft() != null && currentNode.getLeft().getData().contains(String.valueOf(currentCh) ) ) {
+					currentNode = currentNode.getLeft();
+					encodedChar += "0";
+				} else if (currentNode.getRight() != null && currentNode.getRight().getData().contains(String.valueOf(currentCh) ) ) {
+					currentNode = currentNode.getRight();
+					encodedChar += "1";
+				}
+			}
+		}
+		return encodedChar;
 	}
 
 	public static void main (String[] args) {
